8280242bd31d2bc06bfdb95dce240d46e6cb3e9c
AVINMALI2998 <mr.abhi2998@gmail.com>
2025-08-24 10:20:32 +0530

    Another stram class addede

--- src/core_java/Access_specifiers.java
+++ src/core_java/Access_specifiers.java
@@ -3,8 +3,8 @@ package core_java;
 public class Access_specifiers {
 
     public static int a = 10; // Public access specifier
-    private int b = 20; // Private access specifier
-    protected static int c = 30; // Protected access specifier
+   // private int b = 20; // Private access specifier
+    protected int c = 30; // Protected access specifier
    public int d = 40; // Default access specifier (no modifier)
    public static int e = 50; // Static variable, accessible without an object
 
---
+++ src/java_stream/StreamExample.java
@@ -0,0 +1,13 @@
+package java_stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamExample {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .forEach(System.out::println);
+    }
+}
